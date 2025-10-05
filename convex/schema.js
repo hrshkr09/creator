cc877ec12580c9acd94fad60049b48cf732c1f71
Harsh Kumar <hrshkr09@gmail.com>
2025-10-05 17:32:14 +0530

    complete database design and middleware part

--- convex/schema.js
+++ convex/schema.js
@@ -1,5 +1,6 @@
 import {defineSchema, defineTable} from "convex/server"
 import {v} from "convex/values"
+
 export default  defineSchema({
     users: defineTable({
         name : v.string(),
@@ -14,4 +15,83 @@ export default  defineSchema({
         lastActiveAt: v.number(),
 
     }).index("by_token",["tokenIdentifier"]).index("by_email",["email"]).index("by_username",["username"]).searchIndex("search_name",{searchField:"name"}).searchIndex("search_email",{searchField:"email"}),
+
+
+    posts: defineTable({
+        title: v.string(),
+        content: v.string(), //Rich text content (JSON string or HTML)
+        status: v.union(v.literal("draft"),v.literal("published")),
+
+        //Author relationship
+        authorId: v.id("users"),
+
+        //Content metadata
+        tags: v.array(v.string()),
+        category: v.optional(v.string()),//Single category
+        featuredImage: v.optional(v.string()), //ImageKit URL
+
+        //Timestamp
+        createdAt: v.string(),
+        updatedAt: v.string(),
+        publishedAt: v.optional(v.number()),
+        scheduledAt: v.optional(v.number()),// fir scheduled publishing
+
+        //Analytics
+        viewCount: v.number(),
+        likeCount: v.number(),
+
+
+    }).index("by_author",["authorId"])
+    .index("by_status",["status"])
+    .index("by_published",["publishedAt"])
+    .index("by_author_status",["authorId","status"])
+    .searchIndex("search_content",{searchField:"title"}),
+
+
+
+    comments: defineTable({
+        postId: v.id("posts"),
+        authorId:v.optional(v.id("users")),// optional for anonymous comments
+        authorName:v.string(),// for anonymous or display name
+        authorEmail: v.optional(v.string()),//for anonymous comments
+
+        content: v.string(),
+        status: v.union(
+            v.literal("approved"),
+            v.literal("pending"),
+            v.literal("rejected")
+        ),
+
+        createdAt: v.number(),
+    }).index("by_post",["postId"]).index("by_post_status",["postId","status"]).index("by_author",["authorId"]),
+
+
+    likes: defineTable({
+        postId: v.id("posts"),
+        userId: v.optional(v.id("users")),//optional for anonymous likes
+
+        createdAt: v.number(),
+
+    }).index("by_post",["postId"])
+    .index("by_user",["userId"])
+    .index("by_post_user",["postId","userId"]), // prevent duplicate likes
+
+
+    follows: defineTable({
+        followerId: v.id("users"),//user doing the folllowing
+        followingId: v.id("users"),// user being followed
+
+        createdAt: v.number(),
+    }).index("by_follower" ,["followerId"])
+    .index("by_following",["followingId"])
+    .index("by_relationship",["followerId","followingId"]),//prevent duplicates
+
+    dailyStats: defineTable({
+        postId: v.id("postId"),
+        date: v.string(), // YYYY-MM-DD format for easy querying
+        views: v.number(),
+
+    }).index("by_post",["postId"])
+    .index("by_date",["date"])
+    .index("by_post_date",["postId","date"]),//unqiue constraint
 })
--- middleware.js
+++ middleware.js
@@ -1,6 +1,22 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"])
+
+export default clerkMiddleware(async (auth, req)=>{
+  const {userId} = await auth()
+
+  if(!userId && isProtectedRoute){
+    const {redirectToSignIn} = await auth()
+
+    return redirectToSignIn()
+  }
+
+  return NextResponse.next()
+  
+});
+
+
 
 export const config = {
   matcher: [
